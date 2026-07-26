2ae7c7e30a671b21403f0567f89bcb8091ede2b2
saz4871 <deaddeal4871@gmail.com>
2026-07-26 19:36:21 +0500

    Update firebaseconfig.js

--- firebaseconfig.js
+++ firebaseconfig.js
@@ -1,25 +1,21 @@
-//
+ //NOTE: This file is imported by HTML <script type="module"> on the client.
+// Keep it browser-safe (no Node-only APIs).
 
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-database.js";
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBDmjfsZcQ_cqH_SAXBfg6-BIACbs_jtUw",
   authDomain: "banquet-744d0.firebaseapp.com",
-  databaseURL: "https://banquet-744d0-default-rtdb.asia-southeast1.firebasedatabase.app",
+  databaseURL:
+    "https://banquet-744d0-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "banquet-744d0",
   storageBucket: "banquet-744d0.firebasestorage.app",
   messagingSenderId: "648773878860",
   appId: "1:648773878860:web:5480be78312097c3caa0e9",
-  measurementId: "G-QH80XEQH90"
+  measurementId: "G-QH80XEQH90",
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const database = getDatabase(app);
+export default app;
